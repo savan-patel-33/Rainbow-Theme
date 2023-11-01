ab0e150dd0cd3f98fcb4061a3a0f56cd63633638
Savan Godhasara <144118183+savan-patel-33@users.noreply.github.com>
2023-11-01 22:16:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+function colorRed() {
+    document.body.style.background = "red";
+    myNavbar.style.background = "linear-gradient(90deg, rgba(238,10,10,1) 0%, rgba(199,4,27,1) 35%, rgba(255,59,0,1) 100%)";
+    myContent.style.color = "white";
+}
+
+function colorOrange() {
+    document.body.style.background = "orange";
+    myNavbar.style.background = "linear-gradient(90deg, rgba(252,150,5,1) 0%, rgba(249,182,23,1) 50%, rgba(231,181,106,1) 100%)";
+    myContent.style.color = "white";
+}
+
+function colorYellow() {
+    document.body.style.background = "yellow";
+    myNavbar.style.background = "linear-gradient(90deg, rgba(195,199,79,1) 0%, rgba(228,252,58,1) 50%, rgba(121,123,23,1) 100%)";
+    myContent.style.color = "black";
+}
+
+function colorGreen() {
+    document.body.style.background = "green";
+    myNavbar.style.background = "linear-gradient(90deg, rgba(24,190,4,1) 0%, rgba(121,229,9,1) 50%, rgba(173,255,12,1) 100%)";
+    myContent.style.color = "white";
+}
+
+function colorBlue() {
+    document.body.style.background = "blue";
+    myNavbar.style.background = "linear-gradient(90deg, rgba(23,159,225,1) 0%, rgba(79,115,219,1) 35%, rgba(0,181,255,1) 100%)";
+    myContent.style.color = "white";
+}
+
+function colorIndigo() {
+    document.body.style.backgroundColor = "indigo";
+    myNavbar.style.background = "linear-gradient(90deg, rgba(23,17,125,1) 0%, rgba(9,9,121,1) 35%, rgba(8,170,203,1) 100%)";
+    myContent.style.color = "white";
+}
+
+function colorViolet() {
+    document.body.style.background = "violet";
+    myNavbar.style.background = "linear-gradient(90deg, rgba(215,10,159,1) 0%, rgba(219,79,181,1) 35%, rgba(255,0,186,1) 100%)";
+    myContent.style.color = "black";
+
+}
